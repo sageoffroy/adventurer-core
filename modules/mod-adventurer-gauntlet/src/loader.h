d014aa08d3c49f29f8458f54dbc107f4a12ca28b
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-30 01:28:58 -0300

    Register gauntlet run cleanup loader

--- modules/mod-adventurer-gauntlet/src/loader.h
+++ modules/mod-adventurer-gauntlet/src/loader.h
@@ -2,4 +2,5 @@ void AddAdventurerGauntletAccountProgressScripts();
 void AddAdventurerGauntletAccountStashScripts();
 void AddAdventurerGauntletScripts();
 void AddAdventurerGauntletCelebrationScripts();
+void AddAdventurerGauntletRunCleanupScripts();
 void AddAdventurerGauntletSetsScripts();
