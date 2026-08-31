7ff69c89949e0f4b8a0ea118f110f72c658e3afa
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-31 15:08:44 -0300

    feat(gauntlet): load account item collection

--- modules/mod-adventurer-gauntlet/src/loader.h
+++ modules/mod-adventurer-gauntlet/src/loader.h
@@ -2,6 +2,7 @@ void AddAdventurerGauntletScripts();
 void AddAdventurerGauntletPermadeathScripts();
 void AddAdventurerGauntletScalingScripts();
 void AddAdventurerGauntletAccountStashScripts();
+void AddAdventurerGauntletAccountCollectionScripts();
 void AddAdventurerGauntletCelebrationScripts();
 void AddAdventurerGauntletCuratedRewardsScripts();
 void AddAdventurerGauntletSetsScripts();
