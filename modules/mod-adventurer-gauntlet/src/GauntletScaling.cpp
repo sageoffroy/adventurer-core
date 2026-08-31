85def42bff60a41079e68978014b7619397441db
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-31 17:00:37 -0300

    fix(gauntlet): make lone wolf resilient

--- modules/mod-adventurer-gauntlet/src/GauntletScaling.cpp
+++ modules/mod-adventurer-gauntlet/src/GauntletScaling.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <array>
 #include <cstdint>
+#include <unordered_map>
 #include <unordered_set>
 
 namespace
@@ -18,6 +19,7 @@ constexpr uint32 RagefireMapId = 389;
 constexpr uint32 DeadminesMapId = 36;
 constexpr uint32 LoneWolfSpellId = 910501;
 constexpr uint32 LoneWolfDamagePct = 120;
+constexpr uint32 LoneWolfRefreshMs = 1000;
 
 constexpr std::array<uint32, 5> TrashHealthPct = { 50, 150, 200, 250, 300 };
 constexpr std::array<uint32, 5> RareHealthPct  = { 50, 160, 225, 290, 355 };
@@ -25,6 +27,7 @@ constexpr std::array<uint32, 5> BossHealthPct  = { 50, 175, 250, 325, 400 };
 constexpr std::array<uint32, 5> DamagePct      = { 75, 115, 130, 145, 160 };
 
 std::unordered_set<uint64> ScaledCreatures;
+std::unordered_map<uint32, uint32> LoneWolfRefreshTimers;
 
 bool IsGauntletMap(uint32 mapId)
 {
@@ -126,7 +129,7 @@ uint32 ScaleOutgoingDamage(Unit* attacker, uint32 damage)
 
     if (Player* player = attacker->ToPlayer())
     {
-        if (player->HasAura(LoneWolfSpellId))
+        if (IsSoloGauntletPlayer(player))
             return uint32((uint64(damage) * LoneWolfDamagePct) / 100);
         return damage;
     }
@@ -176,11 +179,38 @@ public:
     void OnPlayerMapChanged(Player* player) override
     {
         RefreshLoneWolf(player);
+        if (player)
+            LoneWolfRefreshTimers[player->GetGUID().GetCounter()] = LoneWolfRefreshMs;
     }
 
     void OnPlayerLogin(Player* player) override
     {
         RefreshLoneWolf(player);
+        if (player)
+            LoneWolfRefreshTimers[player->GetGUID().GetCounter()] = LoneWolfRefreshMs;
+    }
+
+    void OnPlayerUpdate(Player* player, uint32 diff) override
+    {
+        if (!player)
+            return;
+
+        uint32 guid = player->GetGUID().GetCounter();
+        uint32& timer = LoneWolfRefreshTimers[guid];
+        if (timer > diff)
+        {
+            timer -= diff;
+            return;
+        }
+
+        timer = LoneWolfRefreshMs;
+        RefreshLoneWolf(player);
+    }
+
+    void OnPlayerLogout(Player* player) override
+    {
+        if (player)
+            LoneWolfRefreshTimers.erase(player->GetGUID().GetCounter());
     }
 };
 
