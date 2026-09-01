b9758790e06dcc01375d9e0abffac56c7eef2f03
Silvio Geoffroy <sageoffroy@gmail.com>
2026-08-31 22:45:50 -0300

    fix(gauntlet): restore stable v2 stash server protocol

--- modules/mod-adventurer-gauntlet/src/AccountStash.cpp
+++ modules/mod-adventurer-gauntlet/src/AccountStash.cpp
@@ -368,10 +368,10 @@ public:
     AdventurerGauntletAccountStashPlayerScript()
         : PlayerScript("AdventurerGauntletAccountStashPlayerScript") { }
 
-    void OnPlayerBeforeSendChatMessage(Player* player, uint32& /*type*/, uint32& /*lang*/, std::string& msg) override
+    void OnPlayerBeforeSendChatMessage(Player* player, uint32& /*type*/, uint32& lang, std::string& msg) override
     {
-        if (player && HandleStashCommand(player, msg))
-            msg.clear();
+        if (player && lang == LANG_ADDON)
+            HandleStashCommand(player, msg);
     }
 };
 
